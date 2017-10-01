b2dfe283dcbaf79e69636284b433bff08f767679
Massy BOURENNANI <cm_bourennani@esi.dz>
2017-10-01 21:53:18 +0200

    implement the reducer with the sort function

--- Recomender.java
+++ Recomender.java
@@ -11,7 +11,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.util.*;
-import java.lang.String;
+import java.lang.*;
 
 
 public class Recomender {
@@ -44,21 +44,53 @@ public class Recomender {
   public static class RecomenderReducer
     extends Reducer<Text, Text, Text, Text> {
 
+        private HashMap<String,Integer> itemsCount;
+
         @Override
         public void reduce(Text key, Iterable<Text> values,Context context)
             throws IOException, InterruptedException {
 
-            HashSet<String> items = new HashSet<String>();
+            // construct the count for each item
+            itemsCount = new HashMap<String,Integer>();
             for(Text item: values){
-              // construct the items for the key
-              items.add(item.toString());
+
+              if(itemsCount.containsKey(item.toString()))
+                itemsCount.put(item.toString(),itemsCount.get(item.toString()).intValue()+1);
+              else
+                itemsCount.put(item.toString(),new Integer(1));
             }
-            // construct the string to store
+            // get an array of items
+            Set<String> itemSet = itemsCount.keySet();
+            String[] items = new String[itemSet.size()];
+            int i=0;
+            for(String item: itemSet){
+              items[i] = item;
+              i++;
+            }
+            // sort the items
+            sortDecreasingly(items);
+            // construct the output of the reduce
             String suggestions = join(items);
+            // write the reduce output value
             context.write(key, new Text(suggestions));
         }
 
-        private String join(Iterable<String> items){
+        private void sortDecreasingly(String[] items){
+
+          Arrays.sort(items, new Comparator<String>() {
+            public int compare(String item1, String item2) {
+                // sort in a decreasing order
+                if (itemsCount.get(item1) > itemsCount.get(item2))
+                   return -1;
+                else if(itemsCount.get(item1) < itemsCount.get(item2))
+                   return +1;
+                else
+                   return 0;
+              }
+          });
+        }
+
+        private String join(String[] items){
             String result = "";
             for(String item: items){
                 result = result +"," + item;
